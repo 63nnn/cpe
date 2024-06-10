2904244fce856c6e717378d6fbd863a4b5793def
63nnn <seanliu0729@gmail.com>
2024-06-10 15:29:42 +0800

    master

--- CPE/a741.10101/main.py
+++ CPE/a741.10101/main.py
@@ -13,33 +13,21 @@ def cal(num1):
     s = num1 // 100
     num1 %= 100
     return num1, s, h, l, k
-    # return k, l, h, s, num1
 
 
-def blank_check():
-    pass
+def index_name(index):
+    list1 = ["shata", "hajar", "lakh", "kuti"]
+    temp = index % 4
+    if temp == 0:
+        return "kuti"
+    else:
+        return list1[temp]
 
 
-def main():
+def sub(c):
     num1 = int(input())
     list1 = [0 for _ in range(11)]
     pointer = 0
-    check = 1
-    while check:
-        temp = cal(num1)
-        for i in range(5):
-            list1[pointer] = temp[i]
-            if i != 4:
-                pointer += 1
-        check = temp[4]
-        num1 = num1 // 10000000
-        print(list1)
-
-
-def test():
-    list1 = [0 for _ in range(11)]
-    pointer = 0
-    num1 = 45897458973958
     check = 1
     while check:
         temp = cal(num1)
@@ -51,7 +39,47 @@ def test():
             i += 1
         check = temp[4]
         num1 = num1 // 10000000
-    print(list1)
+    pointer = 0
+    output = f" {list1[0]}"
+    for i in range(1, len(list1)):
+        if list1[i] != 0:
+            str1 = index_name(pointer)
+            output = f" {list1[i]} {str1}{output}"
+        pointer += 1
+    print(f"{c}.{output}")
 
 
-test()
+def test():
+    num1 = 45897458973958
+    list1 = [0 for _ in range(9)]
+    pointer = 0
+    check = 1
+    while check:
+        temp = cal(num1)
+        i = 0
+        while len(list1) != pointer and len(temp) != i + 1:
+            list1[pointer] = temp[i]
+            if i != 4:
+                pointer += 1
+            i += 1
+        check = temp[4]
+        num1 = num1 // 10000000
+        print(list1)
+    pointer = 0
+    output = f" {list1[0]}"
+    for i in range(1, len(list1)):
+        if list1[i] != 0:
+            str1 = index_name(pointer)
+            output = f" {list1[i]} {str1}{output}"
+        pointer += 1
+    print(output)
+
+
+def main():
+    c = 1
+    while True:
+        sub(c)
+        c += 1
+
+
+main()
