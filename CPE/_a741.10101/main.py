a76fa13e2fd31990efcd5d59b32bd174a6582862
63nnn <seanliu0729@gmail.com>
2024-10-15 18:14:17 +0800

    Update main.py

--- CPE/_a741.10101/main.py
+++ CPE/_a741.10101/main.py
@@ -1,91 +1,57 @@
+# a741. 10101 - Bangla Numbers
+
 # kuti, lakh, hajar, shata
-# 10000000, 100000, 1000, 100
+# 10000000
+# 100000
+# 1000
+# 100
 # 999999999999999
 
 
-def cal(num1):
-    k = num1 // 10000000
-    num1 %= 10000000
-    l = num1 // 100000
-    num1 %= 100000
-    h = num1 // 1000
-    num1 %= 1000
-    s = num1 // 100
-    num1 %= 100
-    return num1, s, h, l, k
-
-
-def index_name(index):
-    list1 = ["shata", "hajar", "lakh", "kuti"]
-    temp = index % 4
-    if temp == 0:
-        return "kuti"
-    else:
-        return list1[temp]
-
-
-def index_namet(index):
-    list1 = ["kuti", "lakh", "hajar", "shata"]
-    temp = -(index % 4)
-    return list1[temp]
-
-
-def sub(c):
-    num1 = int(input())
-    list1 = [0 for _ in range(11)]
-    pointer = 0
-    check = 1
-    while check:
-        temp = cal(num1)
-        i = 0
-        while len(list1) != pointer and len(temp) != i + 1:
-            list1[pointer] = temp[i]
-            if i != 4:
-                pointer += 1
-            i += 1
-        check = temp[4]
-        num1 = num1 // 10000000
-    pointer = 0
-    output = f" {list1[0]}"
-    for i in range(1, len(list1)):
-        if list1[i] != 0:
-            str1 = index_name(pointer)
-            output = f" {list1[i]} {str1}{output}"
-        pointer += 1
-    print(f"{c}.{output}")
-
-
-def test():
-    num1 = 45897458973958
-    list1 = [0 for _ in range(9)]
-    pointer = 0
-    check = 1
-    while check:
-        temp = cal(num1)
-        i = 0
-        while len(list1) != pointer and len(temp) != i + 1:
-            list1[pointer] = temp[i]
-            if i != 4:
-                pointer += 1
-            i += 1
-        check = temp[4]
-        num1 = num1 // 10000000
-        print(list1)
-    pointer = 0
-    output = f" {list1[0]}"
-    for i in range(1, len(list1)):
-        if list1[i] != 0:
-            str1 = index_name(pointer)
-            output = f" {list1[i]} {str1}{output}"
-        pointer += 1
-    print(output)
+def slice_num(x: str):
+    order = [-1, -2, -2, -2]
+    after = []
+    after.append(x[-2:])
+    x = x[:-2]
+    while len(x) > 0:
+        for i in order:
+            if len(x) >= abs(i):
+                tmp = x[i:]
+                if int(tmp) == 0:
+                    tmp = ""
+                after.append(tmp)
+                x = x[:i]
+    return after[::-1]
 
 
 def main():
     c = 1
-    while True:
-        sub(c)
+    ori_num = input()
+    flag = True
+    while ori_num:
+
+        unit = [ "shata", "hajar", "lakh","kuti"]
+
+        sliced = slice_num(ori_num)
+        amount = (len(sliced) - 1) % 4
+        if amount == 0:
+            amount = 4
+        amount -= 1
+        print(f"{c:4}.", end="")
+        while len(sliced) > 1:
+            if amount < 0:
+                amount = 3
+
+            if unit[amount] == "kuti" or sliced[0] != "":
+                print(f" {sliced[0]}", end="")
+                print(f" {unit[amount]}", end="")
+            sliced.remove(sliced[0])
+
+            amount -= 1
+        if sliced[0] != "" and sliced[0] != 00 and sliced[0] != 0:
+            print(f" {sliced[0]}")
         c += 1
+        ori_num = input()
 
 
 main()
